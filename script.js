c3cd5eb200beec3bb665a5927b1a54089df6019c
RLKO (Ruslan Kolani) <RLKO@novonordisk.com>
2024-10-29 18:30:25 +0300

    добавлены комментарии

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // script.js
+
+// Добавляем слушатель события "DOMContentLoaded" для выполнения кода после загрузки DOM
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Загрузка навигационного меню
+  // Загрузка навигационного меню из файла nav.html
   fetch("nav.html")
     .then((response) => response.text())
     .then((data) => {
+      // Вставляем содержимое nav.html в элемент с классом "nav-left-container"
       document.querySelector(".nav-left-container").innerHTML = data;
-      // Добавление класса 'active' к текущему элементу навигации
+
+      // Определяем текущую страницу
       const currentPage = window.location.pathname.split("/").pop();
+      // Получаем все элементы навигации с классом "nav-link"
       const navLinks = document.querySelectorAll(".nav-link");
+
+      // Проходим по каждому элементу навигации
       navLinks.forEach((link) => {
+        // Если href элемента совпадает с текущей страницей, добавляем класс "active"
         if (link.getAttribute("href") === currentPage) {
           link.classList.add("active");
         }
@@ -16,12 +24,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 });
 
+// Функция для отображения увеличенного изображения
 function showEnlargedImage() {
+  // Получаем контейнер для увеличенного изображения по id
   const enlargedImgContainer = document.getElementById(
     "enlarged-img-container"
   );
+  // Вставляем HTML код для увеличенного изображения и подписи
   enlargedImgContainer.innerHTML = `
-          <img src="image.jpg" class="centered-image enlarged-image" alt="Enlarged Image">
-          <div class="image-caption">Хитрый кот</div>
-      `;
+    <img src="image.jpg" class="centered-image enlarged-image" alt="Enlarged Image">
+    <div class="image-caption">Хитрый кот</div>
+  `;
 }
